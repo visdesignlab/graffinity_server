cbbe0b11b105d74483628289eded9892a8d57262
Ethan Kerzner <ethan.kerzner@gmail.com>
2016-11-20 12:17:04 -0700

    Update neo4j.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,7 +2,7 @@ wget -O - https://debian.neo4j.org/neotechnology.gpg.key | apt-key add -
 echo 'deb http://debian.neo4j.org/repo stable/' >/tmp/neo4j.list
 mv /tmp/neo4j.list /etc/apt/sources.list.d
 apt-get -qqy update
-apt-get -qqy install neo4j=2.3.2
+apt-get -qqy install neo4j=2.3.7
 apt-get -qqy install python-flask
 apt-get -qqy install python-pip
 apt-get -qqy install git
